c4d3b28f2b3f143f4ba452c6eb5d07007fd85d62
Mat Tolladay <mjt71@balena-02.cm.balena>
2019-04-10 15:53:34 +0100

    Fiddled the parallel bit

--- ase/calculators/dftb.py
+++ ase/calculators/dftb.py
@@ -177,7 +177,7 @@ class Dftb(FileIOCalculator):
                         offsets[i] += 0.5
                 key = initkey + '_empty%03d' % 3
                 self.parameters[key] = ' '.join(map(str, offsets))
-
+                Nkpts = np.ceil(self.kpts[0]*self.kpts[1]*self.kpts[2]*0.5)
             elif self.kpts_coord is not None:
                 for i, c in enumerate(self.kpts_coord):
                     key = initkey + '_empty%09d'  % i
@@ -256,7 +256,8 @@ class Dftb(FileIOCalculator):
         outfile.write('   IgnoreUnprocessedNodes = Yes  \n')
         outfile.write('} \n')
         outfile.write('Parallel { \n')
-        outfile.write('   UseOmpThreads = Yes  \n')
+        outfile.write('   Groups = 8  \n')
+        outfile.write('   Blacs = 32  \n')
         outfile.write('} \n')
 
 
